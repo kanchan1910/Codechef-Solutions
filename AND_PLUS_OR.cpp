9aa8136ff01b1d8fee531c72f0889e4eab108a3b
Kanchan Jeswani <kanchanjeswani2000@gmail.com>
2020-11-02 01:00:21 +0530

    Solution for October Lunchtime 2020 added.

---
+++ AND_PLUS_OR.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define lli long long int
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        lli x;
+        cin>>x;
+        cout<<0<<" "<<x<<endl;
+    }
+}
